7cab58d2ee217a8feb93718ea2d6e402f69df72c
sukanyasahu123 <sukanya.sahu56@gmail.com>
2026-03-24 19:30:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,31 @@ codes[0].focus();
 
 codes.forEach((code, index) => {
 
-  code.addEventListener("input", () => {
-    if (code.value !== "" && index < 5) {
+  code.addEventListener("input", (e) => {
+    let value = e.target.value;
+
+    if (value.length > 1) {
+      e.target.value = value.slice(0, 1);
+    }
+
+    if (value !== "" && index < codes.length - 1) {
       codes[index + 1].focus();
     }
   });
 
   code.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace" && code.value === "" && index > 0) {
-      codes[index - 1].focus();
+
+    if (e.key === "Backspace") {
+
+      if (code.value !== "") {
+        code.value = "";
+      } 
+      else if (index > 0) {
+        codes[index - 1].focus();
+      }
+
     }
+
   });
 
 });
